727c37dc8e874f34b18eabe36b47f14b4e3d4a5b
root <elhaj102@mail.chapman.edu>
2020-01-25 21:28:59 +0000

    a great template to revist for 2d array testing and understanding next will attempt to complete assignment

--- 2darray.cpp
+++ 2darray.cpp
@@ -3,85 +3,43 @@ using namespace std;
 
 int main() {
 
-  int a[4][5];
+    unsigned int i = 0;
 
-  int const ROWS = 4;
+    unsigned int j = 0;
 
-  int const COLS = 5;
+    int const ROWS = 4;
 
-  int const ELEMENTS = (ROWS * COLS);
+    int const COLS = 5;
 
-  unsigned int i = 0;
+    int const ELEMENTS = (ROWS * COLS);
 
-  unsigned int j = 0;
+         /* 4     5  */
+    int a[ROWS][COLS] = {
+                    {2,2,2,2,2},
+                    {1,1,1,1,1},
+                    {5,5,5,5,5},
+                    {4,4,4,4,4}
+                };
 
-  for(j = 0; j < COLS; ++j){
+    for(i=0; i < ROWS; ++i){
 
-  int valR = 0;
+       for(j=0; j < COLS; ++j){
 
-    for(i = 0; i < ROWS; ++i){
+            cout << a[i][j] << endl;
 
-     int valC = 0;
-
-     cout << "Enter an COL integer: " << endl;
-
-     cin >> valC;
-
-     a[valR][valC];
-
-    }
-
-
-  cout << "Enter an ROW integer: " << endl;
-
-  cin >> valR;
-
-
-  cout << endl;
-
-  }
-
-  for(j = 0; j < ROWS; ++j){
-
-    for(i = 0; i < COLS; ++i){
-
-     cout << a[j][i];
+       }
 
     }
 
-  cout << endl;
+    
 
-  }
+    
 
-  
+    //      cout << "Enter an COL integer: " << endl;
 
 
- /*
+    
 
-  for(j = 0; j < COLS; ++j) {
-
-   int val = 0;
-
-   cout << "Please enter an integer" << endl;
-
-   cin >> val;
-
-   a[0][j]; 
-
-  }
-
-  for(j = 0; j < COLS; ++j){
-
-   cout << a[0][j];
-
-  } */
-
-  // input five inputs for ROW 1
-
-  // input five inputs for ROW 2
-
-  // input five inputs for ROW 3
-
-  return 0;
+    return 0;
 
 }
