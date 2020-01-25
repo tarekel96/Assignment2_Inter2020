eb161f6bb694b25f10dc992805a660702ab8ba3c
root <elhaj102@mail.chapman.edu>
2020-01-25 21:35:25 +0000

    updated the template and now it consoles out cleaner

--- 2darray.cpp
+++ 2darray.cpp
@@ -7,14 +7,21 @@ int main() {
 
     unsigned int j = 0;
 
+    unsigned int k = 0;
+
     int const ROWS = 4;
 
     int const COLS = 5;
 
     int const ELEMENTS = (ROWS * COLS);
 
+    int rowVal = 0;
+
+    int colVal = 0;
+
          /* 4     5  */
     int a[ROWS][COLS] = {
+                
                     {2,2,2,2,2},
                     {1,1,1,1,1},
                     {5,5,5,5,5},
@@ -23,11 +30,17 @@ int main() {
 
     for(i=0; i < ROWS; ++i){
 
+     for(k=0; k < 1; ++k){
+
        for(j=0; j < COLS; ++j){
 
-            cout << a[i][j] << endl;
-
+            cout << a[i][j];
+          
        }
+
+       cout << endl;
+     }
+        
 
     }
 
