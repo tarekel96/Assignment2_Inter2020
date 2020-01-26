1335abb84c14e79909085c14837b1371337db42b
root <elhaj102@mail.chapman.edu>
2020-01-26 17:54:37 +0000

    the second conversion function operates correctly so can now display centimeters to user via invoking methods in main

--- conversion.cpp
+++ conversion.cpp
@@ -5,11 +5,15 @@ void getInput(float *ftL, float *inchesL);
 
 void convertFtToCent(float *ftL);
 
+void convertInchesToCent(float *inchesL);
+
 int main() {
 
     float ft = 0;
 
     float inc = 0.0;
+
+    float combinedInc = 0.0;
 
     cout << "Please enter your height in feet (do not include): " << endl;
 
@@ -23,10 +27,13 @@ int main() {
 
     cout << "You have entered " << ft << "ft. " << inc << " inches." << endl;
 
-
     convertFtToCent(&ft);
 
-    cout << ft << "cm. " << endl;
+    convertInchesToCent(&inc);
+
+    combinedInc = ft + inc;
+
+    cout << combinedInc << "cm. " << endl;
 
     cout << "Program Complete. Goodbye" << endl;
 
@@ -37,10 +44,14 @@ void getInput(float *ftL, float *inchesL) {
 
 }
 
-void convertFtToCent(float *ftL){
-    
+void convertFtToCent(float *ftL){    
     /* 30.48cm in one foot */
     *ftL = *ftL * 30.48;
+    return;
+}
 
+void convertInchesToCent(float *inchesL){   
+    /* 2.54cm in one inch */
+    *inchesL = *inchesL * 2.54;
     return;
 }
